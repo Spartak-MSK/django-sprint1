f9151f26ccc53e54a31774a03ea5cce50682beea
Andrey Drobotov <endrudrobotov@mail.ru>
2025-05-02 03:53:04 +0300

    some change

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('posts/<int:id>/', views.post_detail, name='post_detail'),
     path(
-        'category/<slug:category_slug>/', 
+        'category/<slug:category_slug>/',
         views.category_posts,
         name='category_posts'
     ),
