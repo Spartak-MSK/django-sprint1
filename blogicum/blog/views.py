7f5c02ae48735b7b64eb20fac01d37f5ccc475d7
Andrey Drobotov <endrudrobotov@mail.ru>
2025-05-05 18:27:49 +0300

    big changes (changes in changes)

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -59,5 +59,15 @@ def post_detail(request, id):
 
 
 def category_posts(request, category_slug):
-    filtered_posts = [post for post in posts if post['category'] == category_slug]
-    return render(request, 'blog/category.html', {'category': category_slug, 'posts': filtered_posts})
+    filtered_posts = [
+        post for post in posts
+        if post['category'] == category_slug
+    ]
+    return render(
+        request,
+        'blog/category.html',
+        {
+            'category': category_slug,
+            'posts': filtered_posts
+        }
+    )
